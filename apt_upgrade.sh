29ac47b7bd4eac8bda6744182f508783e11fba0e
PostWarTacos <mwurtz.soc@gmail.com>
2023-08-12 22:43:54 -0500

    Added additional comments

--- apt_upgrade.sh
+++ apt_upgrade.sh
@@ -1,3 +1,6 @@
+# Update package list
 apt update
+# Force upgrade all packages
 apt upgrade -y
+# Remove any unneeded packages
 apt autoremove -y
--- pubkey_create_win.ps1
+++ pubkey_create_win.ps1
@@ -13,7 +13,9 @@ Get-Service ssh-agent
 $key_pair_name = read-host "Enter the name used for key pair:"
 $username = "Enter username on remote PC:"
 $remote_ip = "Enter IP of remote PC"
-# Now load your key files into ssh-agent
+
+# Now load your key files into Windows ssh-agent
 ssh-add $env:USERPROFILE\.ssh\$key_pair_name
 
+# Copy pubkey to remote system
 cat "~/.ssh/$key_pair_name.pub" | ssh "$username@$remote_ip" "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"
